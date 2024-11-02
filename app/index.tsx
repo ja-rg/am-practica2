9b447bb30984e3d34000baf014164f26d0a69f0a
Alejandro Rosales <ja.rg@lasallistas.org.mx>
2024-11-01 20:33:05 -0600

    Refactor login component to include login functionality and API integration

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,71 @@
-import { Colors } from '@/constants/Colors';
-import { Text, View, StyleSheet, Pressable } from 'react-native';
+import { Text, View, Pressable } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { useState } from 'react';
 import { useUser } from '@/context/User';
-import { Link, Stack } from 'expo-router';
+import { Link, router, Stack } from 'expo-router';
+import { styles } from '@/constants/Styles';
+import { Ionicons } from '@expo/vector-icons';
+import * as Crypto from 'expo-crypto';
+import { API } from '@/constants/API';
 
-export default function Home() {
+export default function Login() {
   const { user, setUser } = useUser();
   if (user) {
-    return (
-      <View style={styles.container}>
-        <Stack.Screen options={{ title: 'Inicio de sesión' }} />
-        <Text style={styles.title}>Bienvenido {user.username}</Text>
-      </View>
-    );
+    return <View style={styles.container}>
+      <Stack.Screen options={{ title: 'Inicio de sesión' }} />
+
+      <Ionicons name="checkmark-circle-outline" size={24} style={styles.icon} />
+      <Text style={styles.title}>Ya estás logueado</Text>
+      <Pressable onPress={() => router.replace('/home')} style={styles.button}>
+        <Text style={styles.button_text}>Ir al inicio</Text>
+      </Pressable>
+    </View>
   }
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  function handleLogin() {
-    if (username === 'admin' && password === 'admin') {
-      setUser({ username: 'admin', email: 'admin@admin.com' });
+  function resetFields() {
+    setUsername('');
+    setPassword('');
+  }
+
+  async function handleLogin() {
+    const hashedPassword = await Crypto.digestStringAsync(
+      Crypto.CryptoDigestAlgorithm.SHA256,
+      password
+    );
+
+    const formData = new FormData();
+    formData.append('token', 'code37');
+    formData.append('user', username.replace(/\s+/g, ''));
+    formData.append('pass', hashedPassword);
+    
+
+    const response = await fetch(API.login, {
+      method: 'POST',
+      body: formData
+    });
+
+    if (!response.ok) {
+      return console.error('Error en el inicio de sesión');
     }
 
+    const data = await response.json();
+    if ('error' in data) {
+      return console.error(data.error);
+    }
+
+    setUser(data);
+    router.replace('/home');
+
+    resetFields();
   }
 
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: 'Inicio de sesión' }} />
+      <Ionicons name="person-circle-outline" size={24} style={styles.icon} />
       <Text style={styles.title}>Iniciar Sesión</Text>
       <TextInput
         style={styles.input}
@@ -56,91 +93,3 @@ export default function Home() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Colors.dark.background,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 30,
-    marginBottom: 20,
-    color: Colors.dark.text,
-
-    // Neobrutalism
-    textShadowColor: Colors.dark.shadow,
-    textShadowOffset: { width: 2, height: 2 },
-    textShadowRadius: 1,
-
-    borderBottomColor: Colors.dark.border,
-    borderBottomWidth: 1,
-    padding: 10,
-  },
-  input: {
-    width: '80%',
-    height: 50,
-    marginVertical: 10,
-    padding: 10,
-    backgroundColor: Colors.dark.input,
-    borderRadius: 5,
-    color: Colors.dark.text,
-    fontSize: 16,
-
-    // Neobrutalism
-    borderWidth: 1,
-    borderColor: Colors.dark.border,
-  },
-  button: {
-    width: '80%',
-    height: 40,
-    marginVertical: 10,
-    backgroundColor: Colors.dark.primary,
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-
-    shadowColor: Colors.dark.text,
-    shadowOffset: { width: 3, height: 3 },
-    shadowOpacity: 1,
-    shadowRadius: 1,
-  },
-
-  button_text: {
-    color: Colors.dark.text,
-    fontSize: 16,
-
-    // Shadow
-    textShadowColor: Colors.dark.text,
-    textShadowOffset: { width: .5, height: .5 },
-    textShadowRadius: 2,
-  },
-
-  link_button: {
-    width: '80%',
-    height: 40,
-    marginVertical: 10,
-    backgroundColor: Colors.dark.text,
-    borderRadius: 10,
-
-    justifyContent: 'center',
-    alignContent: 'center',
-
-    textAlign: 'center',
-
-    shadowColor: Colors.dark.primary,
-    shadowOffset: { width: 3, height: 3 },
-    shadowOpacity: 1,
-    shadowRadius: 1,
-  },
-
-  link_button_text: {
-    color: Colors.dark.primary,
-    fontSize: 16,
-
-    // Shadow
-    textShadowColor: Colors.dark.primary,
-    textShadowOffset: { width: .5, height: .5 },
-    textShadowRadius: 2,
-  }
-});
