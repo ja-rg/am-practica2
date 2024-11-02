c8375e8ca98395d52a1fe8f4015e7c72164b99f0
Alejandro Rosales <ja.rg@lasallistas.org.mx>
2024-11-01 20:32:35 -0600

    Add API constants for registration, login, and profile endpoints

---
+++ constants/API.ts
@@ -0,0 +1,7 @@
+const baseURL = 'http://monsterballgo.com/api/';
+
+export const API = {
+    registro: `${baseURL}/register.php`,
+    login: `${baseURL}/login.php`,
+    perfil: `${baseURL}/setpfp.php`,
+};
