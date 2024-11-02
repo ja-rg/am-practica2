192bdfea5dd0ac374564a3dc777f36bacecbf320
Alejandro Rosales <ja.rg@lasallistas.org.mx>
2024-11-01 20:33:19 -0600

    Refactor Home component to include user profile information and logout functionality

---
+++ app/home.tsx
@@ -0,0 +1,40 @@
+import { styles } from "@/constants/Styles";
+import { useUser } from "@/context/User"
+import { Link, Stack, useRouter } from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
+
+import { Text, View, Pressable, TouchableOpacity } from 'react-native';
+import { Image } from "expo-image";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { NotLogged } from "@/components/not-logged";
+
+export default function Home() {
+    const { user, setUser } = useUser()
+    const router = useRouter()
+
+    if (!user) {
+        return <NotLogged />
+    }
+
+    return <View style={styles.container}>
+        <Stack.Screen options={{ title: 'Mi perfil' }} />
+        <TouchableOpacity onPress={() => { alert(`Hola ${user.pfp_url}`) }}>
+            <Image source={user.pfp_url} style={styles.pfp} />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Usuario: {user.firstname}</Text>
+
+        <Text style={styles.label_data}>ID: <Text style={styles.data}>{user.id}</Text></Text>
+        <Text style={styles.label_data}>Nombre: <Text style={styles.data}>{user.firstname} {user.lastname}</Text></Text>
+        <Text style={styles.label_data}>Correo: <Text style={styles.data}>{user.email}</Text></Text>
+        <Text style={styles.label_data}>Experiencia: <Text style={styles.data}>{user.xp}</Text></Text>
+        <Text style={styles.label_data}>Créditos: <Text style={styles.data}>{user.credits}</Text></Text>
+
+        <Pressable onPress={() => { setUser(null); router.replace('/') }} style={styles.button}>
+            <Text style={styles.button_text}>Cerrar sesión</Text>
+        </Pressable>
+        <Link href="/creditos" style={styles.link_button}>
+            <Text style={styles.link_button_text}>Ver créditos</Text>
+        </Link>
+    </View>
+}
